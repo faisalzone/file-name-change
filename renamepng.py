f4f02340be1c033001d96e20d40d6bedd64f83e8
Faisal Ahmed <faisalzone@gmail.com>
2019-04-03 12:13:39 +1100

    First commit

---
+++ renamepng.py
@@ -0,0 +1,42 @@
+import os, xlrd
+
+path = 'C:/users/faisal.ahmed/documents/mystuffs/python-projects/filenamechange' #path to jpg files
+path1 = 'c:/users/faisal.ahmed/Documents/mystuffs/python-projects/filenamechange/somefilename.xlsx'
+
+data =[] #list of jpg filenames in folder
+
+#lets create a filenames list without the jpg extension
+for name in os.listdir(path):
+    fileName, fileExtension = os.path.splitext(name)
+    if fileExtension =='.png':
+        data.append(fileName)
+
+#lets create a list of old filenames in the excel column a
+
+book = xlrd.open_workbook(path1)
+sheet = book.sheet_by_index(0)
+oldNames =[]
+for row in range(sheet.nrows):
+    oldNames.append(sheet.cell_value(row,0))
+
+
+#lets create a list with the new names in column b
+newNames =[]
+for row in range(sheet.nrows):
+    newNames.append(sheet.cell_value(row,1))
+
+
+# now create a dictionary with the old name in a and the corresponding new name in b
+
+fileNames = dict(zip(oldNames,newNames))
+fileNameNotFound = 'not found'
+
+print(fileNames)
+
+#lastly rename your jpg files
+
+for f in data:
+    if f in fileNames.keys():
+        os.rename(path+'/'+f+'.png', path+'/'+fileNames[f]+'.png')
+    if f not in fileNames.keys():
+        os.rename(path+'/'+f+'.png', path+'/'+f+'-'+fileNameNotFound+'.png')
